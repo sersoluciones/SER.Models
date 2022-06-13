a5f7570328371579151e36c21cf7a0abcb62b830
Gustavo Adolfo Morales <tavomorales88@gmail.com>
2022-06-13 14:35:18 -0500

    migrate to net 6

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using SER.Models.SERAudit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SER.Models
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = new UserAgent("");
+            Console.WriteLine(JsonSerializer.Serialize(a)); 
+        }
+    }
+}
